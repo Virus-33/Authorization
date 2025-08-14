3836338e4837d1cb76ec78de4640f4edeb4021dc
Virus-33 <hazhdachka.tv@gmail.com>
2025-08-14 12:51:17 +0700

    added main file and db config

---
+++ backend/app/config/db.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+POSTGRES_DB = os.environ['POSTGRES_DB']
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_HOST = os.environ['POSTGRES_HOST']
+POSTGRES_PORT = os.environ['POSTGRES_HOST']
+
+engine = create_engine(f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/'
+                       f'{POSTGRES_DB}')
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from fastapi .staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
